1bf22644a03394770a014ff4117576d02eb5e0f5
XTWang <xtwang@bogon.twguest.thoughtworks.com>
2015-11-21 17:27:22 +0800

    JS: finish question one

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,35 @@
-function single_element(arr) {
-  
+function firstIndex(arr, item) {
+  var isExist = false;
+  var result;
+  arr.forEach(function (ele, i) {
+    if (ele === item && !isExist) {
+      isExist = true;
+      result = i;
+    }
+  });
+
+  return result;
 }
+
+function lastIndex(arr, item) {
+  var result;
+  arr.forEach(function (ele, i) {
+    if (ele === item) {
+      result = i;
+    }
+  });
+
+  return result;
+}
+
+function single_element(arr) {
+  var result = arr.filter(function (item, i) {
+    return i % 2 === 1;
+  });
+  console.log(result);
+  return result.filter(function (item, i) {
+    return firstIndex(result, item) === lastIndex(result, item);
+  });
+}
+
+//第偶数个元素中，选出不重复的元素
--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,22 @@
 function calculate_median(arr) {
+  var new_arr =  arr.sort(function (item1, item2) {
+    return item1 - item2;
+  }).filter(function (item, i) {
+    return i % 2 === 1;
+  });
 
+  return getMedianNum(new_arr);
 }
+
+function getMedianNum(arr) {
+  var result;
+  if (arr.length % 2 === 1) {
+    result = arr[(arr.length - 1) / 2];
+  }else {
+    result = arr[(arr.length) / 2] + arr[((arr.length) / 2) - 1];
+  }
+
+  return result;
+}
+
+//集合中第偶数个元素的个数为偶数时，计算第偶数个元素的中位数
