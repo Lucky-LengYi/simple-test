9526c01c89f4e5109ccf901898a2651c52fb75e5
XTWang <xtwang@CNtwer-3.local>
2015-11-23 11:50:39 +0800

    JS: use indexOf and lastIndexOf

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,35 +1,8 @@
-function firstIndex(arr, item) {
-  var isExist = false;
-  var result;
-  arr.forEach(function (ele, i) {
-    if (ele === item && !isExist) {
-      isExist = true;
-      result = i;
-    }
-  });
-
-  return result;
-}
-
-function lastIndex(arr, item) {
-  var result;
-  arr.forEach(function (ele, i) {
-    if (ele === item) {
-      result = i;
-    }
-  });
-
-  return result;
-}
-
 function single_element(arr) {
   var result = arr.filter(function (item, i) {
     return i % 2 === 1;
   });
-  console.log(result);
   return result.filter(function (item, i) {
-    return firstIndex(result, item) === lastIndex(result, item);
+    return result.indexOf(item) === result.lastIndexOf(item);
   });
 }
-
-//第偶数个元素中，选出不重复的元素
